b78fedd3a0e5ca750825527eda523a83e06f696a
alexanderelmuratov <elmuratov@gmail.com>
2022-03-07 23:36:07 +0200

    Corrections

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const PhonebookSection = styled.section`
-  width: 600px;
+  width: 480px;
   padding: 20px 20px;
   margin: 20px auto;
   background-color: #EDF1FF;
